8ef9c925069b0052dc9f1e7690d950e2b2090798
Aram Simonian <aram.contacts@gmail.com>
2017-09-14 16:25:11 +0100

    Added an extra question for student's cohort and also output that cohort

--- directory.rb
+++ directory.rb
@@ -17,34 +17,50 @@ def print_header
   puts "--------------------------------"
 end
 
-def print_names(names, starts_with = "", short_than = 999)
-  names.each_with_index do |name, index|
-      puts "#{index + 1} #{name[:name]} (#{name[:cohort]} cohort)" if name[:name].start_with?(starts_with) && name[:name].length < short_than
+def print_names(names, starts_with = "", shorter_than = 999)
+  # names.each_with_index do |name, index|
+  #     puts "#{index + 1} #{name[:name]} (#{name[:cohort]} cohort)" if name[:name].start_with?(starts_with) && name[:name].length < short_than
+  # end
+  index = 0
+  while index < names.count
+      puts "#{index + 1} #{names[index][:name]} (#{names[index][:cohort]} cohort)" \
+          "#{names[index][:country]}" \
+            if names[index][:name].start_with?(starts_with) \
+              && names[index][:name].length < shorter_than
+      index += 1
   end
 end
 
 def print_footer(names)
+  puts
   print "Overall, we have #{names.length} great students"
 end
 
 def input_students
-  puts "Please enter tha names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the student's name"
+  puts "To finish, just hit return twice to any question"
 
   students = []
   name = gets.chomp
   while !name.empty? do
+    cohort = ""
+    puts "...and what cohort are they in?"
+    cohort = gets.chomp
+    break if cohort.empty?
+
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort.to_sym, country: :UK}
+    puts "Now we have #{students.count} student#{'s' if students.count}"
     # get another name from the user
+    puts
+    puts "Next student:"
     name = gets.chomp
   end
   # return the array of students
   students
 end
 
-#students = input_students
+students = input_students
 print_header
-print_names(students, "T", 11)
+print_names(students)
 print_footer(students)
