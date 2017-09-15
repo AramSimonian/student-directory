9d57c02d7e763c23523b00187352ffc92dcd84f5
Aram Simonian <aram.contacts@gmail.com>
2017-09-15 21:45:03 +0100

    Added .gitignore and save_students functionality

--- directory.rb
+++ directory.rb
@@ -15,6 +15,8 @@ def process(selection)
   when "2"
     # show the students
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -26,6 +28,7 @@ def print_menu
   puts "\n\n"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -104,6 +107,18 @@ def input_students
   end
   # return the array of students
   @students
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv","w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
